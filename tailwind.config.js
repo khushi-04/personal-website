5a7f9eb359a2086e939e8ff99e00c56a82bfe838
khushi-04 <khushi.chaudhari@gmail.com>
2024-09-11 19:17:56 -0700

    more testing

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html', // Add this or any other HTML files you're using
+    './about.html', // Example if you have more pages
+    './contact.html', // Another example
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

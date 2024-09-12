e15016161234209b5c044215f0f992810a3fded0
khushi-04 <khushi.chaudhari@gmail.com>
2024-09-11 20:55:06 -0700

    added a new page and new component as set up

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,14 @@
+import AboutMe from '../components/AboutMe'
+
 function App() {
     return (
+      <div>
       <div className="bg-blue-500 text-white p-4">
         <h1 className="text-2xl">Welcome to Khushi Chaudhari's Personal Website</h1>
+      </div>
+      <div classname="bg-blue-100 text-white p-4">
+        AboutMe
+      </div>
       </div>
     );
   }
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,12 @@
+// src/components/MyComponent.js
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <div className="p-4 bg-blue-500 text-white">
+      <h1 className="text-xl">Hello, I am MyComponent!</h1>
+    </div>
+  );
+};
+
+export default AboutMe;
